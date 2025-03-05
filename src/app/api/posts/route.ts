75c060eece3f78d0b5ad406a1d5c1fde7f0b0c15
nemanja <antonijevicnemanja68@gmail.com>
2025-03-05 22:11:41 +0100

    FEAT:: GET & POST method for all posts from db

---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,33 @@
+import { db } from "@/db/db";
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+
+// GET method for posts
+export async function GET() {
+    try{
+        const posts = await db.post.findMany();
+        return NextResponse.json(posts, { status: 200})
+    }
+    catch(err) {    
+        return NextResponse.json(err, { status: 500});
+    }
+}
+// POST method for posts
+export async function POST(req: Request){
+    try{
+        const body = await req.json();
+        const { title, content, authorId } = body;
+        const newPost = await db.post.create({
+            data: { title, content, authorId },
+        });
+
+        if(!title || !content || !authorId){
+            return NextResponse.json({error: "All fields are required!"}, {status: 400});
+        }
+
+        return NextResponse.json(newPost, {status: 201});
+    }
+    catch(err){
+        return NextResponse.json({error: err}, { status: 500});
+    }
+}
