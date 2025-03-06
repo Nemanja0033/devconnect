c150ba2e114e6b6ecc6c704b51ad5525883bdc33
nemanja <antonijevicnemanja68@gmail.com>
2025-03-06 16:14:00 +0100

    FEAT:: route for Auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,48 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { db } from "@/db/db";
+import { compare } from "bcrypt"
+
+export const authOptions: any = {
+    providers: [
+        CredentialsProvider({
+            name: "credentials",
+            credentials: {
+                email: { label: "Email", type: "text", placeholder: "example@mail.com" },
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials?.password) {
+                    throw new Error("All fields are required");
+                }
+
+                const user = await db.user.findUnique({
+                    where: { email: credentials.email },
+                });
+
+                if (!user) {
+                    throw new Error("Invalid credentials"); //if user not found
+                }
+
+                const isPasswordValid = await compare(credentials.password, user.password);
+
+                if (!isPasswordValid) {
+                    throw new Error("Invalid credentials"); // if password is not valid thorw this err
+                }
+
+                return { id: user.id, name: user.name, email: user.email };
+            },
+        }),
+    ],
+    session: {
+        strategy: "jwt",
+    },
+    secret: process.env.NEXTAUTH_SECRET, 
+    pages: {
+        signIn: "/login", // redirect to login page
+    },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
