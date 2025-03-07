5965c2d0886c86f8a7428130b9eb363400056061
nemanja <antonijevicnemanja68@gmail.com>
2025-03-07 16:11:45 +0100

    FEAT:: DELETE author comment endpoint

---
+++ src/app/api/comments/[id]/route.ts
@@ -0,0 +1,57 @@
+import { db } from "@/db/db";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import { authOptions } from "../../auth/[...nextauth]/route";
+
+
+export async function GET(req: Request, { params }: { params: { id: string}}){
+    try{
+        const { id } = params;
+
+        if(!id){
+            return NextResponse.json({ message: "Post ID is required"}, { status: 400});
+        }
+
+        const comments = await db.comment.findMany({
+            where: { id },
+            include: { author: { select: { id: true, name: true}}},
+            orderBy: { createdAt: "desc" },
+        });
+
+        return NextResponse.json(comments, { status: 200});
+    }
+    catch(err){
+        return NextResponse.json({ message: "Error fetching comments", err}, { status: 500});
+    }
+}
+
+export async function DELETE(req: Request, { params }: { params: { id: string}}){
+    try{ 
+        const session: any = await getServerSession(authOptions);
+
+        if(!session?.user){
+            return NextResponse.json({error: "Unauthorized"}, { status: 401});
+        }
+
+        const comment = await db.comment.findUnique({
+            where: { id: params.id }
+        });
+
+        if(!comment){
+            return NextResponse.json({error: "Comment not found"});
+        }
+
+        if(comment.authorId !== session?.user.id){
+            return NextResponse.json({error: "Can only delete your own comments"}, { status: 403});
+        }
+
+        await db.comment.delete({
+            where: { id: params.id },
+        });
+
+        return NextResponse.json({ message: "Comment Deleted Succesfully!"});
+    }
+    catch(err){
+        return NextResponse.json({error: err}, {status: 500});
+    }
+}
