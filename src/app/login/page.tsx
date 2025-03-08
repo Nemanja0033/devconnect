e6448bd1b1a341e97a42552c14da288903ea9fd2
nemanja <antonijevicnemanja68@gmail.com>
2025-03-08 14:59:31 +0100

    FEAT:: dynamic UI

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,16 +13,16 @@ const LoginPage = () => {
             <img src="/logo.png" className="w-45" alt="" />
           </div>
   
-          <div className="mt-12 flex-row px-10 relative bottom-20">
-            <label htmlFor="email" className="font-bold text-purple-800 text-lg">Email</label>
+          <div className="mt-12 flex-row px-10 relative bottom-20 text-purple-950">
+            <label htmlFor="email" className="font-bold text-lg">Email</label>
             <input type="email" name="email" placeholder="Enter your email" className="w-full px-5 border border-gray-300 h-10 rounded-2xl shadow-sm mt-2 mb-2" />
-            <label htmlFor="pass" className="font-bold text-purple-800 text-lg">Password</label>
+            <label htmlFor="pass" className="font-bold text-lg">Password</label>
             <input type="password" name="pass" placeholder="Enter your password" className="w-full mb-2 border px-5 h-10 rounded-2xl border-gray-300 shadow-sm mt-2" />
-            <p>Dont have an account? <span onClick={() => setIsHaveAccount(!isHaveAccaount)} className="text-purple-900 cursor-pointer underline">Register</span></p>
+            <p className="text-gray-500">Dont have an account? <span onClick={() => setIsHaveAccount(!isHaveAccaount)} className="text-purple-900 cursor-pointer underline">Register</span></p>
           </div>
   
           <div className="w-full flex justify-center px-10 relative lg:bottom-10 bottom-15">
-            <button className="text-white bg-purple-800 p-2 w-full rounded-2xl cursor-pointer hover:bg-purple-900 transition-all">Submit</button>
+            <button className="text-white bg-purple-950 p-2 w-full rounded-2xl cursor-pointer hover:bg-purple-900 transition-all">Submit</button>
           </div>
         </div>
       </div>
@@ -36,18 +36,18 @@ const LoginPage = () => {
           <img src="/logo.png" className="w-45" alt="" />
         </div>
 
-        <div className="mt-12 flex-row px-10 relative bottom-20">
-          <label htmlFor="username" className="font-bold text-purple-800 text-lg">Username</label>
+        <div className="mt-12 flex-row px-10 relative bottom-20 text-purple-950">
+          <label htmlFor="username" className="font-bold text-lg">Username</label>
           <input type="email" name="username" placeholder="Enter your email" className="w-full px-5 border border-gray-300 h-10 rounded-2xl shadow-sm mt-2 mb-2" />
-          <label htmlFor="email" className="font-bold text-purple-800 text-lg">Email</label>
+          <label htmlFor="email" className="font-bold text-lg">Email</label>
           <input type="email" name="email" placeholder="Enter your email" className="w-full px-5 border border-gray-300 h-10 rounded-2xl shadow-sm mt-2 mb-2" />
-          <label htmlFor="pass" className="font-bold text-purple-800 text-lg">Password</label>
+          <label htmlFor="pass" className="font-bold text-lg">Password</label>
           <input type="password" name="pass" placeholder="Enter your password" className="w-full mb-2 border px-5 h-10 rounded-2xl border-gray-300 shadow-sm mt-2" />
-          <p>Arleady have account? <span onClick={() => setIsHaveAccount(!isHaveAccaount)} className="text-purple-900 cursor-pointer underline">Login</span></p>
+          <p className="text-gray-500">Arleady have account? <span onClick={() => setIsHaveAccount(!isHaveAccaount)} className="text-purple-900 cursor-pointer underline">Login</span></p>
         </div>
 
         <div className="w-full flex justify-center px-10 relative lg:bottom-10 bottom-15">
-          <button className="text-white bg-purple-800 p-2 w-full rounded-2xl cursor-pointer hover:bg-purple-900 transition-all">Submit</button>
+          <button className="text-white bg-purple-950 p-2 w-full rounded-2xl cursor-pointer hover:bg-purple-900 transition-all">Submit</button>
         </div>
       </div>
     </div>
