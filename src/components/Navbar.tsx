046d56f0ac450f73ded438e7c3b09ebbde3da6af
nemanja <antonijevicnemanja68@gmail.com>
2025-03-08 14:12:00 +0100

    FEAT:: dynamic navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,29 @@
 "use client"
+import { useSession } from "next-auth/react"
 
 const Navbar = () => {
+    const { status } = useSession();
+
   return (
     <header className="w-full h-[60px] shadow-md rounded-md flex justify-between items-center">
         <div>
             <img src="/logo.png" className="w-32 h-auto" alt="" />
         </div>
+        <nav className="flex items-center justify-around">
+            {
+                status === 'authenticated' ?
+                (
+                    <>
+                    <button>Create a post</button>
+                    <button>Sign Out</button>
+                    </>
+                )
+                :
+                (
+                    <button>Sign In</button>
+                )
+            }
+        </nav>
     </header>
   )
 }
