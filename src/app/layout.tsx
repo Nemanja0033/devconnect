f34da20f0288778276b5644ab26e1980ee97a722
nemanja <antonijevicnemanja68@gmail.com>
2025-03-08 14:32:05 +0100

    instaling next fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return <SessionProvider>
             <html>
                 <body>
-                    <Navbar />
                     {children}
                 </body>
             </html>
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
-
+"use client"
 const page = () => {
   return (
-    <div>login</div>
+    <div className='w-full h-screen flex justify-center items-center'>
+      <div className='lg:w-1/3 w-[90%] h-[80%] shadow-md flex-row px-5 py-5'>
+        <div className="flex justify-center w-full'">
+          <img src="/logo.png" className="w-32" alt="" />
+        </div>
+        <div className="mt-12 flex-row">
+          <label htmlFor="email" className="font-bold text-purple-800 text-lg">Email</label>
+          <input type="email" name="email" className="w-full border border-gray-300 shadow-sm mt-2" />
+        </div>
+      </div>
+    </div>
   )
 }
 
