592d05248c92d6305c7361cdc1ea10013a893d15
nemanja <antonijevicnemanja68@gmail.com>
2025-03-08 22:18:43 +0100

    FEAT:: error displaying

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,6 +8,7 @@ const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
+  const [error, setError] = useState(false);
 
   const handleLogin = async () => {
     const response = await fetch('/api/login', {
@@ -24,7 +25,7 @@ const LoginPage = () => {
     if (response.ok) {
         await signIn("credentials", { email, password, callbackUrl: '/'})
     } else {
-      alert('error')
+      setError(true);
     }
 };
 
@@ -40,7 +41,12 @@ const handleRegister = async () => {
       password: password
     })
   });
-  location.href = '/login'
+  if(response.ok){
+    location.href = '/login';
+  }
+  else{
+    setError(true);
+  }
 }
 
 
@@ -55,12 +61,13 @@ const handleRegister = async () => {
           <div className="mt-12 flex-row px-10 relative bottom-20 text-purple-950">
             <label htmlFor="email" className="font-bold text-lg">Email</label>
             <input onChange={(e) => setEmail(e.target.value)} type="email" name="email" placeholder="Enter your email" className="w-full px-5 border border-gray-300 h-10 rounded-2xl shadow-sm mt-2 mb-2" />
+            {error ? <p className="text-red-500 text-sm">Invalid Email or Password!</p> : null}
             <label htmlFor="pass" className="font-bold text-lg">Password</label>
             <input onChange={(e) => setPassword(e.target.value)} type="password" name="pass" placeholder="Enter your password" className="w-full mb-2 border px-5 h-10 rounded-2xl border-gray-300 shadow-sm mt-2" />
             <p className="text-gray-500">Dont have an account? <span onClick={() => setIsHaveAccount(!isHaveAccaount)} className="text-purple-900 cursor-pointer underline">Register</span></p>
           </div>
   
-          <div className="w-full flex justify-center px-10 relative lg:bottom-10 bottom-15">
+          <div className="w-full flex justify-center px-10 relative lg:bottom-15 bottom-15">
             <button onClick={handleLogin} className="text-white bg-purple-950 p-2 w-full rounded-2xl cursor-pointer hover:bg-purple-900 transition-all">Submit</button>
           </div>
         </div>
@@ -80,6 +87,7 @@ const handleRegister = async () => {
           <input onChange={(e) => setUsername(e.target.value)} type="text" name="username" placeholder="Enter your email" className="w-full px-5 border border-gray-300 h-10 rounded-2xl shadow-sm mt-2 mb-2" />
           <label htmlFor="email" className="font-bold text-lg">Email</label>
           <input onChange={(e) => setEmail(e.target.value)} type="email" name="email" placeholder="Enter your email" className="w-full px-5 border border-gray-300 h-10 rounded-2xl shadow-sm mt-2 mb-2" />
+          {error ? <p className="text-red-500">Invalid Email or Password!</p> : null}
           <label htmlFor="pass" className="font-bold text-lg">Password</label>
           <input onChange={(e) => setPassword(e.target.value)} type="password" name="pass" placeholder="Enter your password" className="w-full mb-2 border px-5 h-10 rounded-2xl border-gray-300 shadow-sm mt-2" />
           <p className="text-gray-500">Arleady have account? <span onClick={() => setIsHaveAccount(!isHaveAccaount)} className="text-purple-900 cursor-pointer underline">Login</span></p>
