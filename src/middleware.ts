9c13cfb1da4011e941bbef7565526e1a561fae59
nemanja <antonijevicnemanja68@gmail.com>
2025-03-09 20:54:41 +0100

    protecting createpost route

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,5 +7,5 @@ export default withAuth({
 });
 
 export const config = {
-    matcher: ["/dashboard", "/dashboard/:path", "/profile", "/profile/:path"]
+    matcher: ["/dashboard", "/dashboard/:path", "/createpost", "/profile", "/profile/:path"]
 };
