89aa20a462191d4a7fb855185d07fadf6a87b3a6
nemanja <antonijevicnemanja68@gmail.com>
2025-03-09 22:13:21 +0100

    FEAT:: dymaic UI, sound effect and handling succesfull post

--- src/app/createpost/page.tsx
+++ src/app/createpost/page.tsx
@@ -1,33 +1,52 @@
 "use client"
-import { CheckCircle2Icon, Image } from "lucide-react"
+import { CheckCircle2Icon, CircleCheck, Image } from "lucide-react"
 import { uploadToCloud } from "../helpers/uploadImage";
 import React, { useEffect, useRef, useState } from "react";
 import { useSession } from "next-auth/react";
+import { useLoadingStore } from "@/store/useLoadingStore";
+import Loader from "@/components/ui/Loader";
+import Link from "next/link";
 
 const CreatePostPage = () => {
     const { data: session } = useSession();
     const titleRef = useRef<HTMLInputElement | null>(null);
+    const postedSound = new Audio('/posted.mp3');
+
     const [img, setimg] = useState("");
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
     const [isImageLoaded, setIsImageLoaded] = useState(false); // state for displaying succes message if image is uploaded succesfully
+    const { isLoading, toggleLoading } = useLoadingStore();
+    const [isPosted, setIsPosted] = useState(false);
 
     useEffect(() => {
         titleRef.current?.focus();
     }, []);
 
+    const handleSuccesfullPost = async () => {
+        await postedSound.play();
+        toggleLoading();
+        setIsPosted(true);
+    }
+
     const createPost = async () => {
-        try{
-            await fetch("/api/posts", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ title, content, img, }),
-              });
-        }
-        catch(err){
-            alert("Something went wrong!");
-            console.log(err)
-        }
+       if(title.length < 1 || content.length < 1 || img.length < 1){
+        alert("All fields are required!")
+       }
+       try{
+        toggleLoading();
+        await fetch("/api/posts", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ title, content, img, }),
+          });
+          handleSuccesfullPost();
+    }
+    catch(err){
+        toggleLoading();
+        alert("Something went wrong!");
+        console.log(err)
+    }
     }
 
     const handleFileUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -35,13 +54,27 @@ const CreatePostPage = () => {
         if (!file) return;
 
         try {
+            toggleLoading();
             const url = await uploadToCloud(file);
             setimg(url);
             setIsImageLoaded(true);
+            toggleLoading();
         } catch (error) {
-            console.error('Error uploading image:', error);
+            toggleLoading();
+            alert('Error uploading image:');
         }
     };
+
+  if(isPosted){
+    return(
+        <div className="w-full h-screen flex justify-center lg:px-40 px-5">
+            <section className="flex-row lg:w-1/2 w-full">
+                <h1 className="text-3xl font-semibold text-purple-900">Posted Successfully!</h1>
+                <Link className="bg-purple-800 hover:bg-purple-900 transition-all rounded-2xl p-2 text-white font-semibold cursor-pointer" href={"/"}>Home</Link>
+            </section>
+        </div>
+    )
+  }
 
   return (
     <div className="w-full h-screen flex justify-center lg:px-40 px-5">
@@ -59,10 +92,12 @@ const CreatePostPage = () => {
                         accept="image/*"  
                         onChange={handleFileUpload}
                         />
-                {isImageLoaded ? <span className="flex items-center relative top-1 ml-1 gap-1 text-green-700">Image Uploaded! <CheckCircle2Icon /></span> : null}
+                <div className="relative top-1 ml-1">
+                    {!isLoading && isImageLoaded ? <span className="text-green-400 text-md">Image Uploaded!</span> : isLoading && !isImageLoaded ? <Loader /> : null}
+                </div>
             </div>
             <textarea name="content" onChange={(e) => setContent(e.target.value)} placeholder="Post Content. . ." className="w-full mt-3 px-5 min-h-64 max-h-64 rounded-2xl border border-gray-300"></textarea>
-            <button onClick={createPost} className="bg-purple-800 w-full hover:bg-purple-900 transition-all rounded-2xl p-2 mt-3 text-white font-semibold cursor-pointer">Submit Post</button>
+            <button onClick={createPost} className="bg-purple-800 w-full hover:bg-purple-900 transition-all rounded-2xl p-2 mt-3 text-white font-semibold flex justify-center items-center cursor-pointer">{isLoading ? <Loader /> : 'Submit Post'}</button>
         </section>
     </div>
   )
