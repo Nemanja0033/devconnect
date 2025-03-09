4fffeacc0b07093c531c5c9dccd89acb8b8eba39
nemanja <antonijevicnemanja68@gmail.com>
2025-03-09 23:09:54 +0100

    FIX:: fixing deploy issue with exported authOptions

---
+++ src/lib/authOptions.ts
@@ -0,0 +1,60 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import { db } from "@/db/db";
+import { compare } from "bcrypt";
+import { NextAuthOptions } from "next-auth";
+
+// auth options cannot be inside next-auth api route 
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "credentials",
+            credentials: {
+                email: { label: "Email", type: "text", placeholder: "example@mail.com" },
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials?.password) {
+                    throw new Error("All fields are required");
+                }
+
+                const user = await db.user.findUnique({
+                    where: { email: credentials.email },
+                });
+
+                if (!user) {
+                    throw new Error("Invalid credentials");
+                }
+
+                const isPasswordValid = await compare(credentials.password, user.password);
+
+                if (!isPasswordValid) {
+                    throw new Error("Invalid credentials");
+                }
+
+                return { id: user.id, name: user.name, email: user.email };
+            },
+        }),
+    ],
+    session: {
+        strategy: "jwt",
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    pages: {
+        signIn: "/login",
+    },
+    callbacks: {
+        async jwt({ token, user }: any) {
+            if (user) {
+                token.id = user.id;
+            }
+            return token;
+        },
+        async session({ session, token }: any) {
+            if (session.user) {
+                session.user.id = token.id;
+            }
+            return session;
+        },
+    },
+};
