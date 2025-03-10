b967072ef4e80be496fefdc9905c34e36c22dd7f
nemanja <antonijevicnemanja68@gmail.com>
2025-03-10 15:10:03 +0100

    FEAT:: ISR feed page

--- src/app/(feed)/page.tsx
+++ src/app/(feed)/page.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import { db } from "@/db/db"; 
 
-export default async function FeedPage(){
+// ISR for refreshing cached data every x seconds
+
+export const revalidate = 60; 
+
+async function fetchPosts() {
+  return await db.post.findMany({
+    orderBy: { createdAt: "desc" },
+    select: {
+      id: true,
+      title: true,
+      img: true,
+      createdAt: true,
+      _count: {
+        select: {
+          Like: true, 
+          Comment: true, 
+        },
+      },
+    },
+  });
+}
+
+
+export default async function FeedPage() {
+  const posts = await fetchPosts();
+
   return (
-    <div>Hello</div>
-  )
+    <main className="w-full h-screen flex px-5 justify-center">
+      <section className="lg:w-[80%] w-full flex-row p-5 rounded-md border-x border-gray-50 mt-3 shadow-lg">
+        {posts.length > 0 ? (
+          posts.map((post) => (
+            <div>
+            </div>
+          ))
+        ) : (
+          <p>No posts found.</p>
+        )}
+      </section>
+    </main>
+  );
 }
