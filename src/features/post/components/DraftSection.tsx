d8cfd63c2cd5c831a2c653719d6171456ca98e3b
Nemanja Antonijevic <antonijevicnemanjaa@gmail.com>
2025-10-02 20:37:33 +0200

    refactor drafts component

---
+++ src/features/post/components/DraftSection.tsx
@@ -0,0 +1,34 @@
+import Draft from '@/features/post-drafts/components/Draft';
+import { DraftSkeleton } from '@/features/post-drafts/components/DraftSkeleton';
+import { mapDraftsToNumberArray } from '@/features/post-drafts/lib/lib';
+import { PostDraftType, ProjectDraftType } from '@/features/post-drafts/types';
+import React from 'react'
+
+const DraftSection = ({ isDraftsLoading, drafts, openEditDraftModal, setIsDeleteDraftModalOpen, setCurrentDraft }: any) => {
+  return (
+    <div className="w-full h-full overflow-auto">
+              {isDraftsLoading ? <DraftSkeleton exsistingDrafts={mapDraftsToNumberArray(drafts)} /> : (
+                drafts.map((draft: PostDraftType | ProjectDraftType) => (
+                  <div className="w-full mt-3" key={draft.id}>
+                    <Draft onEditClick={() => openEditDraftModal(draft.id)} 
+                           onDeleteClick={() => {
+                            setIsDeleteDraftModalOpen(true);
+                            setCurrentDraft(draft);
+                           }}
+                           title={draft.title} 
+                           type={draft.type} 
+                    />
+                  </div>
+                ))
+              )}
+
+              {!isDraftsLoading && drafts.length === 0 && (
+                <div className="w-full h-96 flex justify-center items-center">
+                  <span className="text-md text-primary">No drafts to show</span>
+                </div>
+              )}
+    </div>
+  )
+}
+
+export default DraftSection
